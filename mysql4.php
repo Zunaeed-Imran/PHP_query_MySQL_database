2fe08891d83ce8b95b62c7ab12b6980fcee84e10
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-11-25 14:36:39 +0600

    Update mysql4.php
    add comment

--- mysql4.php
+++ mysql4.php
@@ -4,7 +4,7 @@
   $sql = "SELECT * FROM users = 'Sandy'";
   $result =  mysqli_query($conn, $sql);
 
-  if(mysqli_num_rows($result) > 0){
+  if(mysqli_num_rows($result) > 0){     // The mysqli_num_rows() function returns the number of rows in a result set.
       $row = mysqli_fetch_assoc($result);
       echo $row["id"] . "</br>";
       echo $row["user"] . "</br>";
